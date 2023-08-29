cc20f718698b8f1f65caf293def120cd5cb3d73d
9elt <nelt.primary@gmail.com>
2023-08-29 21:02:48 +0200

    practise sheet refactory

--- src/content/practise/index.js
+++ src/content/practise/index.js
@@ -1,51 +1,47 @@
 import { State, createNode } from "@9elt/miniframe";
 import practiseCSS from "../../styles/practise.css";
 import history from "../../util/history";
-import { isKanji } from "../../util";
+
 
 export default async function renderPractiseSheet() {
     await history.ok;
-    const active = hidden.as(h => history.value.filter(k => !h.includes(k.kanji)));
 
-    document.head.append(createNode(style));
-    document.body.append(createNode(body(active, history.value)));
-}
+    const _ref = structuredClone(history.value);
+    const _order_init = history.value.map(flatten);
 
-const hidden = State.from([]);
+    const order = State.from("");
 
-const style = {
-    tagName: "style",
-    innerHTML: practiseCSS,
-};
+    const available = order.as(str => _ref.sort(sortAs(str || _order_init)));
 
-const body = (active, history) => ({
-    tagName: "body",
-    children: [
-        { tagName: "br" },
-        header(active),
-        controls(history),
-        { tagName: "br" },
-        {
-            tagName: "div",
-            children: history.map(kanji => info(kanji)),
-        },
-    ],
-});
+    const hidden = State.from("");
 
-const header = (active) => {
-    const mean = active.as(a => {
-        let mean = 0;
-        let count = 0;
-        for (let i = 0; i < a.length; i++) {
-            const jlpt = parseInt(a[i].jlpt.charAt(1));
-            if (isNaN(jlpt)) { continue; }
-            count++;
-            mean += jlpt;
-        }
-        return Math.round(mean / count) || "/A";
+    const active = State.use({
+        a: available,
+        h: hidden
+    }).as(({ a, h }) => !h ? a : a.filter(k => !h.includes(k.kanji)));
+
+    const printRefText = State.from(true);
+
+    const style = {
+        tagName: "style",
+        innerHTML: practiseCSS,
+    };
+
+    const body = () => ({
+        tagName: "body",
+        children: [
+            { tagName: "br" },
+            header(),
+            controls(),
+            { tagName: "br" },
+            {
+                tagName: "div",
+                children: available.as(a => a.map(info)),
+            },
+        ],
     });
 
-    return {
+    const header = () => ({
         tagName: "header",
         children: [
             {
@@ -59,143 +55,265 @@ const header = (active) => {
             { tagName: "br" },
             {
                 tagName: "p",
-                children: ["kanji: ", active.as(a => a.map(k => k.kanji).join(" ") + ` (${a.length})`)]
-            },
-            {
-                tagName: "p",
-                children: ["mean jlpt: N", mean],
-            },
-            {
-                tagName: "p",
                 children: ["created: ", (new Date).toLocaleString("en", {
                     day: "numeric",
                     month: "long",
                     year: "numeric"
                 })]
             },
+            {
+                tagName: "p",
+                children: ["mean jlpt: N", active.as(jlptMean)],
+            },
+            { tagName: "br" },
+            {
+                tagName: "p",
+                children: [
+                    "kanji ",
+                    active.as(a => `(${a.length}):`),
+                    { tagName: "br" },
+                    active.as(a => a.map(flatten).join(" ")),
+                ]
+            },
+            { tagName: "br" },
+            {
+                tagName: "p",
+                className: State.use({ p: printRefText, o: order })
+                    .as(({ p, o }) => p && o ? "ref-text" : "ref-text hidden"),
+                children: [
+                    "reference text: ",
+                    { tagName: "br" },
+                    {
+                        tagName: "div",
+                        children: order.as(str => str.split("\n")
+                            .map(p => p ? { tagName: "p", children: [p] } : { tagName: "br" }))
+                    }
+                ],
+            },
+            { tagName: "br" },
+            { tagName: "hr" },
             { tagName: "br" },
         ]
-    };
-}
+    });
 
-const controls = (history) => ({
-    tagName: "div",
-    className: "controls",
-    children: [
-        { tagName: "hr" },
-        { tagName: "br" },
-        {
-            tagName: "button",
-            children: ["PRINT"],
-            onclick: () => window.print(),
-        },
-        { tagName: "br" },
-        { tagName: "br" },
-        {
-            tagName: "p",
-            children: ["available kanji: ", history.map(k => k.kanji).join(" ")],
-        },
-        { tagName: "br" },
-        {
-            tagName: "div",
-            className: "flex",
-            children: [{
-                tagName: "div",
-                style: { width: "100%" },
+    const controls = () => ({
+        tagName: "div",
+        className: "controls",
+        children: [
+            { tagName: "br" },
+            {
+                tagName: "button",
+                children: ["PRINT"],
+                onclick: () => window.print(),
+            },
+            { tagName: "br" },
+            { tagName: "br" },
+            {
+                tagName: "p",
                 children: [
+                    "available kanji ",
+                    available.as(a => `(${a.length}):`),
+                    { tagName: "br" },
                     {
-                        tagName: "textarea",
-                        placeholder: "Enter here the kanji you don't want to print",
-                        rows: 5,
-                        children: [""],
-                        oninput: (e) => {
-                            hidden.value = e.target?.value?.split("").filter(isKanji) ?? [];
-                        },
+                        tagName: "small",
+                        children: [
+                            "The kanji currently in your history",
+                        ],
                     },
-                ]
-            }],
-        },
-        { tagName: "br" },
-        { tagName: "hr" },
-    ]
-});
-
-const info = (kanji) => ({
-    tagName: "div",
-    className: hidden.as(h => h.includes(kanji.kanji)
-        ? "hidden wrapper" : "wrapper"),
-    children: [
-        {
-            tagName: "div",
-            className: "strokes",
-            children: [
-                dash({
+                    { tagName: "br" },
+                    available.as(a => a.map(flatten).join(" ")),
+                ],
+            },
+            { tagName: "br" },
+            {
+                tagName: "div",
+                className: "flex",
+                children: [{
                     tagName: "div",
-                    className: "ref",
+                    style: { width: "100%" },
                     children: [
                         {
                             tagName: "p",
-                            className: "jlpt",
-                            children: ["JLPT ", { tagName: "b", children: [kanji.jlpt] }]
+                            children: [
+                                "removed kanji: ",
+                                { tagName: "br" },
+                                {
+                                    tagName: "small",
+                                    children: [
+                                        "The kanji listed here won't be printed",
+                                    ],
+                                }
+                            ]
+                        },
+                        { tagName: "br" },
+                        {
+                            tagName: "textarea",
+                            placeholder: "Enter some kanji",
+                            rows: 3,
+                            children: [""],
+                            oninput: ({ target }) => {
+                                hidden.value = target?.value || "";
+                            },
+                        },
+                        { tagName: "br" },
+                        {
+                            tagName: "p",
+                            children: [
+                                "reference text: ",
+                                { tagName: "br" },
+                                {
+                                    tagName: "small",
+                                    children: [
+                                        "Some text containing the kanji in your history.",
+                                        { tagName: "br" },
+                                        "The kanji will be printed in the same order as they appear in the reference text."
+                                    ],
+                                }
+                            ]
+                        },
+                        { tagName: "br" },
+                        {
+                            tagName: "textarea",
+                            placeholder: "Enter some reference text",
+                            rows: 8,
+                            children: [""],
+                            oninput: ({ target }) => {
+                                order.value = target?.value || "";
+                            },
                         },
                         {
-                            tagName: "h1",
-                            children: [kanji.kanji],
-                        },
+                            tagName: "div",
+                            children: [{
+                                tagName: "input",
+                                type: "checkbox",
+                                checked: printRefText.value,
+                                name: "include-reference",
+                                onchange: ({ target }) => {
+                                    printRefText.value = target.checked;
+                                }
+                            }, {
+                                tagName: "label",
+                                for: "include-reference",
+                                children: [" print reference text"]
+                            }],
+                        }
                     ]
-                }),
-                ...kanji.strokes.map(stroke => dash(stroke))
-            ],
-        },
-        {
-            tagName: "div",
-            className: "info",
-            children: [
-                { tagName: "br" },
-                {
-                    tagName: "div",
-                    className: "flex",
-                    style: { width: "100%" },
-                    children: [
-                        list(kanji.readings.kun, "kun-readings"),
-                        list(kanji.readings.on, "on-readings")
-                    ]
-                },
-                { tagName: "br" },
-                list(kanji.meanings, "meanings"),
-            ]
-        },
-        { tagName: "br" },
-        {
-            tagName: "div",
-            className: "practice",
-            children: [dash(""), dash(""), dash(""), dash(""), dash(""), dash(""), dash(""), dash(""), dash(""), dash("")],
-        }
-    ]
-});
+                }],
+            },
+            { tagName: "br" },
+            { tagName: "br" },
+            { tagName: "hr" },
+        ]
+    });
 
-const list = (readings, type) => ({
-    tagName: "div",
-    className: type.includes("-")
-        ? type.replace(/^[^-]*-/, "")
-        : type,
-    children: [
-        {
-            tagName: "h4",
-            children: [type.replace("-", " ") + ":"],
-        },
-        {
-            tagName: "ul",
-            children: readings.slice(0, 8).map(r => ({
-                tagName: "li",
-                children: [r]
-            }))
-        }
-    ]
-});
+    const info = (kanji) => ({
+        tagName: "div",
+        className: hidden.as(h => h.includes(kanji.kanji)
+            ? "hidden wrapper" : "wrapper"),
+        children: [
+            {
+                tagName: "div",
+                className: "strokes",
+                children: [
+                    dash({
+                        tagName: "div",
+                        className: "ref",
+                        children: [
+                            {
+                                tagName: "p",
+                                className: "jlpt",
+                                children: ["JLPT ", { tagName: "b", children: [kanji.jlpt] }]
+                            },
+                            {
+                                tagName: "h1",
+                                children: [kanji.kanji],
+                            },
+                        ]
+                    }),
+                    ...kanji.strokes.map(stroke => dash(stroke))
+                ],
+            },
+            {
+                tagName: "div",
+                className: "info",
+                children: [
+                    // { tagName: "br" },
+                    {
+                        tagName: "div",
+                        className: "flex",
+                        style: { width: "100%" },
+                        children: [
+                            list(kanji.readings.kun, "kun-readings"),
+                            list(kanji.readings.on, "on-readings")
+                        ]
+                    },
+                    // { tagName: "br" },
+                    list(kanji.meanings, "meanings"),
+                ]
+            },
+            { tagName: "br" },
+            {
+                tagName: "div",
+                className: "practice",
+                children: [
+                    dash(), dash(), dash(),
+                    dash(), dash(), dash(),
+                    dash(), dash(), dash(),
+                    dash(),
+                ],
+            }
+        ]
+    });
 
-const dash = (child) => ({
-    tagName: "div",
-    className: (child.className ?? "") + " dash",
-    children: [child || ""]
-});
+    const list = (readings, type) => ({
+        tagName: "div",
+        className: type.includes("-")
+            ? type.replace(/^[^-]*-/, "")
+            : type,
+        children: [
+            {
+                tagName: "h4",
+                children: [type.replace("-", " ") + ":"],
+            },
+            {
+                tagName: "ul",
+                children: readings.slice(0, 8).map(r => ({
+                    tagName: "li",
+                    children: [r]
+                }))
+            }
+        ]
+    });
+
+    const dash = (child) => ({
+        tagName: "div",
+        className: (child?.className ?? "") + " dash",
+        children: [child || ""]
+    });
+
+    function jlptMean(a) {
+        let mean = 0;
+        let count = 0;
+        for (let i = 0; i < a.length; i++) {
+            const jlpt = parseInt(a[i].jlpt.charAt(1));
+            !isNaN(jlpt) && (count++) && (mean += jlpt);
+        }
+        return Math.round(mean / count) || "/A";
+    }
+
+    function sortAs(str) {
+        return (a, b) => iOf(str, a.kanji) - iOf(str, b.kanji);
+    }
+
+    function iOf(str, char) {
+        let i = str.indexOf(char);
+        return i < 0 ? str.length : i;
+    }
+
+    function flatten(a) {
+        return a.kanji
+    }
+
+    document.head.append(createNode(style));
+    document.body.append(createNode(body()));
+}
